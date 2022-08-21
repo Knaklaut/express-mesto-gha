ef5d7d4f4c7cd2212ab6fb9477706a5aa4f76593
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-08-21 22:39:40 +0300

    ПР13 fix: Внести исправления по результатам теста.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes');
+const cardRouter = require('./routes/cards');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
 
 app.use((req, res, next) => {
   req.user = {
@@ -15,6 +15,9 @@ app.use((req, res, next) => {
   };
   next();
 });
+
+app.use('/cards', cardRouter);
+app.use('/users', userRouter);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Страница не найдена.' });
