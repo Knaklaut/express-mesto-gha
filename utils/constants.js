02e759b02ce6dc1c073e47c81ab689a4ac5bfc9e
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-08-22 13:21:01 +0300

    ПР13 refactor: Исправить ошибки по итогам 1-го ревью.

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,6 @@
-const CREATED = 201;
-const BAD_REQUEST = 400;
-const NOT_FOUND = 404;
-const SERVER_ERROR = 500;
-
 module.exports = {
-  CREATED,
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
 };
