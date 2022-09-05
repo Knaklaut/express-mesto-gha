6e240af7406caf0a9d2044057087c3368a0e2f64
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-09-05 22:03:38 +0300

    ПР14 feat: check additions

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,9 +4,4 @@ const SECRET_KEY = 'some-secret-key';
 
 const generateToken = (payload, lifetime) => jwt.sign(payload, SECRET_KEY, { expiresIn: lifetime });
 
-const checkToken = (token) => jwt.verify(token, SECRET_KEY);
-
-module.exports = {
-  generateToken,
-  checkToken,
-};
+module.exports = generateToken;
