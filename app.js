cc7bd8841ad82d965438e4fbbbc848a77de0f4a2
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-09-05 23:35:19 +0300

    ПР14 feat: check additions

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 
-const auth = require('./middlewares/auth');
-const notFoundPage = require('./middlewares/notFoundPage');
-const errProcessor = require('./middlewares/errProcessor');
 const { validationAuth, validationUser } = require('./middlewares/validityCheck');
+const auth = require('./middlewares/auth');
+const errProcessor = require('./middlewares/errProcessor');
+const notFoundPage = require('./middlewares/notFoundPage');
 const createUser = require('./routes/newUserRouter');
 const login = require('./routes/login');
 const usersRouter = require('./routes/users');
@@ -15,18 +15,17 @@ const cardsRouter = require('./routes/cards');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(auth);
+app.use(notFoundPage);
 app.use('/signin', validationAuth, login);
 app.use('/signup', validationUser, createUser);
-
-app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-app.use(notFoundPage);
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(errors());
 app.use(errProcessor);
