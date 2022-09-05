aae80ddd69879120cf98e14464b75ab696f36829
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-09-05 21:54:55 +0300

    ПР14 feat: check additions

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const AuthError = require('../errors/AuthError');
 const { checkToken } = require('../utils/jwt');
 
 module.exports = (req, res, next) => {
-  const auth = req.headers.authorization;
+  const { auth } = req.headers;
   if (!auth) {
     next(new AuthError('Необходима авторизация.'));
   }
