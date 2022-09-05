917d185dd86619b7ff8a296e5a197e1ba10f1c95
Sergey Ermilov <serjio.ermilov@gmail.com>
2022-09-06 00:12:07 +0300

    ПР14 feat: check additions

--- utils/regExp.js
+++ utils/regExp.js
@@ -1,7 +1,3 @@
-const linkCheck = (link) => {
-  const regExp = /(https?:\/\/)(w{3}\.)?([a-zA-Z0-9-]{0,63}\.)([a-zA-Z]{2,4})(\/[\w\-._~:/?#[\]@!$&'()*+,;=]#?)?/;
-  if (regExp.test(link)) return link;
-  throw new Error('Некорректная ссылка.');
-};
+const regExp = /(https?:\/\/)(www\.)?([a-zA-Z0-9-]{0,63}\.)([a-zA-Z]{2,4})(\/[\w\-._~:/?#[\]@!$&'()*+,;=]#?)?/;
 
-module.exports = { linkCheck };
+module.exports = regExp;
